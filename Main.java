efbaba4e78166241a2242d25d2906f165729f563
tchluc <tchamdjalucas@gmail.com>
2026-01-17 14:47:32 +0000

    first

---
+++ Main.java
@@ -0,0 +1,11 @@
+import view.GameApplication;
+
+/**
+ * Point d'entree principal du jeu Colony Power.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        GameApplication.main(args);
+    }
+}
